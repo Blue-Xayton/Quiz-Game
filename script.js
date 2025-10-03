d2a32fd5ebbf56571d4c9eef505c702120ae0988
Blu3 Xayt0n <ikagengmpolokeng@gmail.com>
2025-10-03 11:28:35 +0200

    Update script.js
    update the comment answer for python

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const quizQuestions = [
     answers: [
       { text: "//", correct: false },
       { text: "<!-- -->", correct: false },
-      { text: "#", correct: true },
+      { text: "#", correct:true },
       { text: "/* */", correct: false },
     ],
   },
@@ -208,5 +208,6 @@ function restartQuiz() {
   startQuiz();
 
 }
+
 
 
